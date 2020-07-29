216013541dad3920995ff8d58f4a48d6b08f7e8f
duanjinhui <1813932878@qq.com>
2020-07-29 10:27:52 +0800

    修正 bg跟随镜头

--- Assets/Scripts/BGCtrl.cs
+++ Assets/Scripts/BGCtrl.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 
 public class BGCtrl : MonoBehaviour
 {
-    public Transform followCameraTrans;
+    public Transform cameraTrans;
     private Vector3 fcLastFramePos;
     private float fcOriginY;
     // Start is called before the first frame update
     private void Start()
     {
-        fcOriginY = followCameraTrans.position.y;
-        fcLastFramePos = followCameraTrans.position;
+        fcOriginY = cameraTrans.position.y;
+        fcLastFramePos = cameraTrans.position;
     }
     Vector3 offset=Vector3.zero;
+    Vector3 targetPos=Vector3.zero;
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        if(followCameraTrans.position.y>= fcOriginY)
-        {
-            offset.y = followCameraTrans.position.y - fcLastFramePos.y;
-            transform.position += offset;
-            fcLastFramePos = followCameraTrans.position;
-        }
-        
+        //offset.x = cameraTrans.position.x - fcLastFramePos.x;
+        //offset.y = cameraTrans.position.y - fcLastFramePos.y;
+        //// Debug.Log(offset);
+        //transform.position += offset;
+        //fcLastFramePos = cameraTrans.position;
+        targetPos.x = cameraTrans.position.x;
+        targetPos.y = cameraTrans.position.y;
+        transform.position = targetPos;
     }
 }
